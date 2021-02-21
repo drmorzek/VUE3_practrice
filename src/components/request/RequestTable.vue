8f36affecc106469ae58862c1eff007771ec4957
drmorzek <drmorzek@mail.ru>
2021-02-21 19:18:17 +0300

    next tick

---
+++ src/components/request/RequestTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <table>
+      <thead>
+          <tr>
+              <th>#</th>
+              <th>ФИО</th>
+              <th>Телефон</th>
+              <th>Сумма</th>
+              <th>Статус</th>
+              <th>Действие</th>
+          </tr>
+      </thead>
+  </table>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ui/AppPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+      <h1 class="card-title">
+          {{ title }}
+          <slot name="header" />
+      </h1>
+
+      <slot />
+  </div>
+</template>
+
+<script>
+import { computed, watch } from 'vue'
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props){
+    document.title = `${props.title} | Клон банка`
+  }
+}
+</script>
+
+<style>
+
+</style>
